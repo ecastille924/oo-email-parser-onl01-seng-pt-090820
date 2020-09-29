f9583e4e42cd2fe159e9b901ab7b705c09dfb08b
Erick Castille <ecastille924@gmail.com>
2020-09-29 23:50:09 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,10 +7,10 @@ class EmailParser
  
   def parse 
     csv_emails.split.collect do |address|
-      address.split(',')
-      end
+      address.split(',') 
     end
-    flatten.uniq
-  end
+    .flatten.uniq 
+  end 
+end 
   
 end
