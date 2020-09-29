54622047624f9ec5edb14ae2eb34352300d8c176
Erick Castille <ecastille924@gmail.com>
2020-09-29 23:57:25 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,12 +1,12 @@
 class EmailParser
   attr_accessor :email_addresses
   
-  def initialize (email_addresses)
+  def initialize(email_addresses)
     @email_addresses = email_addresses
   end
  
   def parse
-    email_array = @email_addresses.split(/[, ]/).uniq
+    email_array = @email_addresses.split(',').uniq
     email_array.reject! {|element| element.empty?}
     email_array
   end
